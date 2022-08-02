81c0e3fd1d7cbf47dc8ec41bf3a0dca5db08d7d0
Trident12kilo <dhdlsrnr1231@naver.com>
2022-08-02 19:03:25 +0900

    22/08/02 프로젝트 중간 업로드

---
+++ src/main/java/com/hk3fg/board/domain/repository/BoardRepository.java
@@ -0,0 +1,10 @@
+package com.hk3fg.board.domain.repository;
+
+import com.hk3fg.board.domain.entity.BoardEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
+    List<BoardEntity> findByTitleContaining(String keyword);
+}
