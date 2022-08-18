f82a946906f6fa32cc108bcb3ac8d787adeec80d
Trident12kilo <dhdlsrnr1231@naver.com>
2022-08-18 23:59:32 +0900

    22/08/16 이전커밋 추가 / 스프링 시큐리티에 의한 parameterName 값 문제를 해결해 보았다 (정답인지는 모른다)

--- src/main/resources/static/js/write.js
+++ src/main/resources/static/js/write.js
@@ -1,7 +1,16 @@
-<script>
-    $(document).ready(function() {
-    $('#temp').click(function() {
-        $('#append').val($('#tt').text());
-    })
-});
-</script>
+/*$(document).ready(function() {*/
+    $('#btn_submit').click(function() {
+        const title_value = document.getElementById('title').value.trim();
+        alert(title_value);
+        if(title_value == ""){
+            alert('제목을 입력해주세요');
+            $('title').focus();
+            return false;
+        } else {
+            $('#append').val($('#test').html());
+            $("form_write").submit();
+        }
+
+    });
+/*
+});*/
