40edb703f486588b018535528db5c4a1aa503311
InGukOh <87802273+InGukOh@users.noreply.github.com>
2022-09-01 23:37:16 +0900

    22/09/01 점수데이터 가져오기 성공!!!!

--- src/main/java/com/hk3fg/board/config/SecurityConfig.java
+++ src/main/java/com/hk3fg/board/config/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests()
+        http
+                .authorizeRequests()
                 .antMatchers("/**")
                 .permitAll()
                 .and()
