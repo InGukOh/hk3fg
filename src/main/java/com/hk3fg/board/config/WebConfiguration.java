2c8cb6b64ddfb9c8e99ccd606a062bf3318bfa46
InGukOh <87802273+InGukOh@users.noreply.github.com>
2022-09-09 02:21:03 +0900

    22/09/09 배포 성공!!! / http://dhdlsrnr1232.cafe24.com/  부트스트랩 5.1.3 사용중 / 반응형 시도중

--- src/main/java/com/hk3fg/board/config/WebConfiguration.java
+++ src/main/java/com/hk3fg/board/config/WebConfiguration.java
@@ -16,12 +16,12 @@ public class WebConfiguration implements WebMvcConfigurer {
         registry.addResourceHandler("/common/**").addResourceLocations("classpath:/templates/common/").setCachePeriod(60 * 60 * 24 * 365);
         /* '/js/**'로 호출하는 자원은 '/static/js/' 폴더 아래에서 찾는다. */
         registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/").setCachePeriod(60 * 60 * 24 * 365);
-        /* '/bootstrap-5.1.3-dist/**'로 호출하는 자원은 '/static/bootstrap-5.1.3-dist/' 폴더 아래에서 찾는다. */
-        registry.addResourceHandler("/static/bootstrap-5.1.3-dist/**").addResourceLocations("classpath:/static/bootstrap-5.1.3-dist/").setCachePeriod(60 * 60 * 24 * 365);
-        /* '/css/**'로 호출하는 자원은 '/static/css/' 폴더 아래에서 찾는다. */
-        registry.addResourceHandler("/static/css/**").addResourceLocations("classpath:/static/css/").setCachePeriod(60 * 60 * 24 * 365);
+        /* '/static/bootstrap-5.1.3-dist/**'로 호출하는 자원은 '/static/bootstrap-5.1.3-dist/' 폴더 아래에서 찾는다. */
+        registry.addResourceHandler("/bootstrap-5.1.3-dist/**").addResourceLocations("classpath:/static/bootstrap-5.1.3-dist/").setCachePeriod(60 * 60 * 24 * 365);
+        /* '/static/css/**'로 호출하는 자원은 '/static/css/' 폴더 아래에서 찾는다. */
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/").setCachePeriod(60 * 60 * 24 * 365);
         /* '/img/**'로 호출하는 자원은 '/static/img/' 폴더 아래에서 찾는다. */
-        registry.addResourceHandler("/img/**").addResourceLocations("classpath:/image_Game/").setCachePeriod(60 * 60 * 24 * 365);
+        registry.addResourceHandler("/img/**").addResourceLocations("classpath:/static/img/").setCachePeriod(60 * 60 * 24 * 365);
 
         //registry.addResourceHandler("/board/**").addResourceLocations("classpath:/templates/board/").setCachePeriod(60 * 60 * 24 * 365);
         registry.addResourceHandler("/Bungtermelon_Marge/**").addResourceLocations("classpath:/Game_BungterMelon_Marge/").setCachePeriod(60 * 60 * 24 * 365);
